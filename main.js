44333060b82f1bab80516bb976f4d6a6fd6ab70c
minorusso <gumbomb1@gmail.com>
2022-07-13 19:10:28 +0900

    heroスライダーのアニメーションを自動再生

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
-  effect: 'coverflow',
+// addEventListener:ターゲットに特定のイベントが配信されるたびに呼び出される関数を設定
+// DOMContentLoaded:最初の HTML 文書の読み込みと解析が完了したとき、スタイルシート、画像、サブフレームの読み込みが完了するのを待たずに発生
+// document.addEventListener('DOMContentLoaded', function () {
+//   // インスタンスかする際に引数をわたす
+//   const hero = new HeroSlider('.swiper');
+//   hero.start();
+//   setTimeout(() => {
+//     hero.stop();
+//   }, 5000);
+// });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  const hero = new HeroSlider('.swiper');
+  hero.start();
 });
